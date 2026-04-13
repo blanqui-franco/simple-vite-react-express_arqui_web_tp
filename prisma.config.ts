e7ed5506eb34f89fb7dbe461d241a6a608f0eaba
blanqui-franco <blanquisaldivar9@gmail.com>
2026-04-13 09:26:51 -0300

    se cambiaa prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -32,16 +32,8 @@ export default defineConfig({
 import path from "node:path";
 import { defineConfig } from "prisma/config";
 
-const databaseUrl = process.env.DATABASE_URL;
-
 export default defineConfig({
     schema: path.join(import.meta.dirname, "prisma", "schema.prisma"),
-    ...(databaseUrl && {
-        datasource: {
-            url: databaseUrl,
-        },
-    }),
 });
-
 
 
